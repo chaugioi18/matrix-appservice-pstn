fe303ebe172c3e69560b3018ad1ae1e57f452fc2
mila <nguyenngocchau18@gmail.com>
2023-07-28 12:01:10 +0700

    Add log

--- src/appservice.ts
+++ src/appservice.ts
@@ -56,6 +56,10 @@ export function createAppservice(options: IAppserviceOptions): Appservice {
 
         return (appservice as any).handleThirdpartyObject(req, res, "user", req.query["userid"] as string);
     });
+    appservice.on('thirdparty.protocol', async (protocol, fields, cb) => {
+        console.log(`Received ${protocol} protocol query`, fields);
+        cb()
+    })
 
     appservice.on('thirdparty.user.remote', async (protocol, fields, cb) => {
         console.log(`Received ${protocol} remote query`, fields);
