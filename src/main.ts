8f0d1393f6730a09678feb3a80f94633060e2aab
mila <nguyenngocchau18@gmail.com>
2023-08-15 11:19:19 +0700

    init proxy

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
-import { Invitation } from "sip.js";
+import {Invitation} from "sip.js";
 import Call from './Call'
-import { getIntentInRoom } from "./store";
-import { createAppservice, getOrUploadAvatarUrl } from "./appservice";
-import { formatPhoneNumber } from './utils';
-import { APPSERVICE_CONFIG, COUNTRY_CODE } from './config';
+import {getIntentInRoom} from "./store";
+import {createAppservice, getOrUploadAvatarUrl} from "./appservice";
+import {formatPhoneNumber} from './utils';
+import {APPSERVICE_CONFIG, COUNTRY_CODE} from './config';
 // import { createUserAgent } from './sip';
 
 var sip = require('sip')
+var proxy = require('sip/proxy')
 
 // mapping between Call-IDs and Call instances
-const callMapping: {[callId: string]: Call} = {}
+const callMapping: { [callId: string]: Call } = {}
 
 /**
  * Called when we recieve an invite from freeswitch
@@ -20,19 +21,19 @@ async function onInvite(invitation: Invitation) {
 
     // prepend contry code?
     let from = invitation.request.from.displayName
-    if(from.startsWith('0') && !from.startsWith('00')) {
-        from = COUNTRY_CODE+from.slice(1)
+    if (from.startsWith('0') && !from.startsWith('00')) {
+        from = COUNTRY_CODE + from.slice(1)
     }
-    if(from.startsWith('00')) {
-        from = '+'+from.slice(2)
+    if (from.startsWith('00')) {
+        from = '+' + from.slice(2)
     }
 
-    if(!matrixId) {
+    if (!matrixId) {
         await invitation.reject()
         console.error('got invite, but without any matrixId', {matrixId, callId, from})
         return
     }
-    if(!from.slice(1).match(/^[0-9]+/)) {
+    if (!from.slice(1).match(/^[0-9]+/)) {
         await invitation.reject()
         console.error('got invite, but From/Caller-ID seems invalid', {matrixId, callId, from})
         return
@@ -47,22 +48,22 @@ async function onInvite(invitation: Invitation) {
     // is there already a room with that number and that matrix ID?
     const rooms = await intent.getJoinedRooms()
     let roomId: string
-    for(let r of rooms) {
+    for (let r of rooms) {
         const members = await intent.underlyingClient.getJoinedRoomMembers(r)
         console.log(r, {members})
-        if(members.includes(matrixId)) {
+        if (members.includes(matrixId)) {
             roomId = r
             break
         }
     }
 
     // if not, create one
-    if(!roomId) {
+    if (!roomId) {
         roomId = await intent.underlyingClient.createRoom({
             preset: 'private_chat',
             name: formatPhoneNumber(from),
             is_direct: true,
-            invite: [ matrixId ]
+            invite: [matrixId]
         })
     }
 
@@ -82,7 +83,7 @@ const appservice = createAppservice(APPSERVICE_CONFIG)
 appservice.on("room.event", async (roomId, event) => {
     console.log(`EVENTTTTTTTTTTTTT ${JSON.stringify(event)}`)
     // is it a event sent by the appservice?
-    if(appservice.getSuffixForUserId(event["sender"])) {
+    if (appservice.getSuffixForUserId(event["sender"])) {
         // ignore
         return
     }
@@ -94,16 +95,22 @@ appservice.on("room.event", async (roomId, event) => {
 
     // let's find an intent which is able to post in that room
     const intent = await getIntentInRoom(roomId, appservice)
-    if(!intent) {
+    if (!intent) {
         console.error(`we could not find any way to participate in room ${roomId} after recieving an '${event.type}' event`)
         return
     }
-    function rstring() { return Math.floor(Math.random()*1e6).toString(); }
-    function randnum() { Math.floor(Math.random() * 1e5) }
+
+    function rstring() {
+        return Math.floor(Math.random() * 1e6).toString();
+    }
+
+    function randnum() {
+        Math.floor(Math.random() * 1e5)
+    }
 
     let call: Call
     try {
-        switch(event["type"]) {
+        switch (event["type"]) {
 
             // Invite to a new call by the matrix user
             case 'm.call.invite':
@@ -142,15 +149,13 @@ appservice.on("room.event", async (roomId, event) => {
                         //     'a=ptime:30\r\n'+
                         //     'a=sendrecv\r\n'
                     },
-                    function(rs) {
+                    function (rs) {
                         console.log(`RS!!!!! ${JSON.stringify(rs)}`)
-                        if(rs.status >= 300) {
+                        if (rs.status >= 300) {
                             console.log('call failed with status ' + rs.status);
-                        }
-                        else if(rs.status < 200) {
+                        } else if (rs.status < 200) {
                             console.log('call progress status ' + rs.status);
-                        }
-                        else {
+                        } else {
                             // yes we can get multiple 2xx response with different tags
                             console.log('call answered with tag ' + rs.headers.to.params.tag);
                             // const content = {
@@ -181,16 +186,15 @@ appservice.on("room.event", async (roomId, event) => {
                             });
 
                             var id = [rs.headers['call-id'], rs.headers.from.params.tag, rs.headers.to.params.tag].join(':');
-                            if(!dialogs[id]) {
-                                dialogs[id] = function(rq) {
-                                    if(rq.method === 'BYE') {
+                            if (!dialogs[id]) {
+                                dialogs[id] = function (rq) {
+                                    if (rq.method === 'BYE') {
                                         console.log('call received bye');
 
                                         delete dialogs[id];
 
                                         sip.send(sip.makeResponse(rq, 200, 'Ok'));
-                                    }
-                                    else {
+                                    } else {
                                         sip.send(sip.makeResponse(rq, 405, 'Method not allowed'));
                                     }
                                 }
@@ -212,28 +216,28 @@ appservice.on("room.event", async (roomId, event) => {
             // SDP candidates
             case 'm.call.candidates':
                 call = callMapping[callId]
-                if(!call) return
+                if (!call) return
                 await call.handleCandidates(event)
                 break
 
             // matrix user accepts the out call invite
             case 'm.call.answer':
                 call = callMapping[callId]
-                if(!call) return
+                if (!call) return
                 await call.handleAnswer(event)
                 break
 
             // matrix user hangs up the call
             case 'm.call.hangup':
                 call = callMapping[callId]
-                if(!call) return
+                if (!call) return
                 call.hangup()
                 break
 
             case 'm.room.message':
                 console.log(`Received message ${event["event_id"]} from ${event["sender"]} in ${roomId}: ${event["content"]["body"]}`);
-            }
-    } catch(err) {
+        }
+    } catch (err) {
         console.error(err)
         // intent.sendText(roomId, 'Error processing the call:\n'+err.message, 'm.notice')
     }
@@ -243,23 +247,44 @@ var dialogs = {};
 async function main() {
     // await userAgent.start()
     // console.log('sip connected')
-    sip.start({}, function (rq) {
-        console.log(`SIP START ${JSON.stringify(rq)}`)
-        sip.send(sip.makeResponse(rq, 200, "OK"));
-        // if(rq.headers.to) { // check if it's an in dialog request
-        //     var id = [rq.headers['call-id'], rq.headers.to.params.tag, rq.headers.from.params.tag].join(':');
-        //     if(dialogs[id])
-        //         dialogs[id](rq);
-        //     else
-        //         sip.send(sip.makeResponse(rq, 481, "Call doesn't exists"));
-        //     console.log(`call id ${id}`)
-        // }
-        // else{
-        //     console.log(`Method not allowed`)
-        //     sip.send(sip.makeResponse(rq, 405, 'Method not allowed'));
-        // }
-        console.log("SIP END")
-    });
+    // sip.start({}, function (rq) {
+    //     console.log(`SIP START ${JSON.stringify(rq)}`)
+    //     sip.send(sip.makeResponse(rq, 200, "OK"));
+    //     // if(rq.headers.to) { // check if it's an in dialog request
+    //     //     var id = [rq.headers['call-id'], rq.headers.to.params.tag, rq.headers.from.params.tag].join(':');
+    //     //     if(dialogs[id])
+    //     //         dialogs[id](rq);
+    //     //     else
+    //     //         sip.send(sip.makeResponse(rq, 481, "Call doesn't exists"));
+    //     //     console.log(`call id ${id}`)
+    //     // }
+    //     // else{
+    //     //     console.log(`Method not allowed`)
+    //     //     sip.send(sip.makeResponse(rq, 405, 'Method not allowed'));
+    //     // }
+    //     console.log("SIP END")
+    // });
+    proxy.start({
+            logger: {
+                recv: function (m) {
+                    console.log('recv:' + JSON.stringify(m));
+                },
+                send: function (m) {
+                    console.log('send:' + JSON.stringify(m));
+                },
+                error: function (e) {
+                    console.log(e.stack);
+                }
+            }
+        }, function (rq) {
+            if (rq.method === 'OPTION') {
+                var rs = sip.makeResponse(rq, 200, 'Ok');
+                proxy.send(rs);
+            } else {
+                proxy.send(sip.makeResponse(rq, 404, 'Not Found'));
+            }
+        }
+    )
     await appservice.begin()
     console.log('appservice is up!')
 }
