5ad2e7e74a7d62f198b9d23025938ed53143ca48
mila <nguyenngocchau18@gmail.com>
2023-12-01 09:20:24 +0700

    Update url

--- src/sip.ts
+++ src/sip.ts
@@ -4,11 +4,11 @@ import { Registerer, UserAgent, Invitation } from "sip.js";
 import SessionDescriptionHandler from './SessionDescriptionHandler';
 
 const userAgent = new UserAgent({
-    uri: UserAgent.makeURI("sip:C03847979@SIP16224.worldfone.vn"),
+    uri: UserAgent.makeURI("sip:C03847979@sip16224.worldfone.vn"),
     authorizationUsername: 'C03847979',
     authorizationPassword: 'C0384@966d',
     transportOptions: {
-        server: "wss://SIP16244.worldfone.vn:7779/ws",
+        server: "wss://sip16244.worldfone.vn:7779/ws",
     },
     sessionDescriptionHandlerFactory: (session, options) => {
         return new SessionDescriptionHandler()
